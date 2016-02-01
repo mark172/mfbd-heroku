2d1a3a81c971adae7512e477e2ae0dbaa95d4a6f
Mark Evangelista <mark172@me.com>
2016-02-01 02:25:14 +0000

    added contacts scaffolding

---
+++ app/views/contacts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @contact, :id, :first_name, :last_name, :email, :phone, :message, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129033616) do
+ActiveRecord::Schema.define(version: 20160201004609) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
